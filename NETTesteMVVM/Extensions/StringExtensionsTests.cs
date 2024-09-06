7d7e2ebadbd9168acd6f3b17000aea40b063d1a5
igorbdaluz <igorbdaluz@hotmail.com>
2024-09-06 14:10:34 -0300

    Criando teste para a extension de nome de formulário.

---
+++ NETTesteMVVM/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using VisualBasicLib.Extensions;
+
+namespace NETTesteMVVM.Extensions;
+
+public class StringExtensionsTests
+{
+    [Test]
+    public void ToFormNameTest()
+    {
+        var nomeFormularioPessoa = "Pessoa".ToFormName();
+        var nomeFormularioEndereco = "Endereço".ToFormName();
+
+        Assert.That(nomeFormularioPessoa, Is.EqualTo("frmPessoa"));
+        Assert.That(nomeFormularioEndereco, Is.EqualTo("frmEndereco"));
+        Assert.Pass();
+    }
+}
