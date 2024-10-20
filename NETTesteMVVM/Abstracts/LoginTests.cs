3d70f3f86e85e8f201060c7200559849b1600546
igorbdaluz <igorbdaluz@hotmail.com>
2024-10-20 14:51:08 -0300

    Criando o teste para o login.

---
+++ NETTesteMVVM/Abstracts/LoginTests.cs
@@ -0,0 +1,33 @@
+﻿using EntityFrameworkLib.Context;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Moq;
+using VisualBasicLib.Abstracts;
+using VisualBasicLib.Interfaces;
+
+namespace NETTesteMVVM.Abstracts;
+
+public class LoginTests
+{
+    private Mock<LoginAbstract> _mockLogin;
+    private Mock<INavigationManager> _mockNavigation;
+    private Mock<LibDbContext> _mockContext;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockNavigation = new Mock<INavigationManager>();
+        _mockContext = new Mock<LibDbContext>();
+        _mockLogin = new Mock<LoginAbstract>(_mockNavigation.Object, _mockContext.Object);
+    }
+
+    [Test]
+    public void HashPasswordTest()
+    {
+        var login = _mockLogin.Object;
+        var hashPassword = login.HashPassword("teste", [0,1,1,0]);
+        var isCorrect = login.VerifyPassword("teste", hashPassword, [0, 1, 1, 0]);
+
+        Assert.IsTrue(isCorrect);
+        Assert.Pass();
+    }
+}
